553936a1f010d7f2d7c7517b503c90953c14f8d0
dpurcell <dpurcell.pd@gmail.com>
2023-07-12 17:47:17 -0400

    objects created

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 startGame = () => {
-    console.log("Game started!");
+    
+    class USSAssembly {
+        constructor(hull = 20, firepower = 5, accuracy = .7) {
+            this.hull = hull;
+            this.firepower = firepower;
+            this.accuracy = accuracy;
+        }
+    }
+
+    class AlienShip {
+        constructor(
+            hull = Math.floor(randomizeStat(3 , 6)),
+            firepower = Math.floor(randomizeStat(2, 4)),
+            accuracy = (randomizeStat(.6, .8)).toFixed(1)
+        ) {
+            this.hull = hull;
+            this.firepower = firepower;
+            this.accuracy = accuracy;
+        }
+    }
+
+    const ENEMY = new AlienShip();
+    console.log(`New enemy ship appeared!
+    Hull: ${ENEMY.hull} Firepower: ${ENEMY.firepower} Accuracy: ${ENEMY.accuracy}`);
+   
+    
+}
+
+
+randomizeStat = (min, max) => {    
+    return Math.random() * (max - min) + min;
 }
 
 const GAME_BTN = document.getElementById("game-btn");

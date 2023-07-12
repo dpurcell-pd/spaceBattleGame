0dfb9fa8a12c7faac6d4274ba55078ed2004f5e6
dpurcell <dpurcell.pd@gmail.com>
2023-07-12 18:23:51 -0400

    expanded prompt and initial attack

--- script.js
+++ script.js
@@ -28,13 +28,20 @@ startGame = () => {
     `;
     GAME_SECTION.appendChild(GAME_TEXT);
     setTimeout(() => {
-        GAME_TEXT.innerHTML += `Looks like we're gonna have to skip the pleasantries...we've got enemy ships inboud!`
+        GAME_TEXT.innerHTML += `Looks like we're gonna have to skip the pleasantries...we've got enemy ships inboud!
+        <br><br>
+        
+        Take a moment to get your console open so you can get a read on their stats, and when you're ready...press the button to engage the first target!`
+
+        GAME_SECTION.appendChild(ATTACK_BTN);
     }, 2000);
 
     const ENEMY = new AlienShip();
     console.log(`New enemy ship appeared!
-    Hull: ${ENEMY.hull} Firepower: ${ENEMY.firepower} Accuracy: ${ENEMY.accuracy}`);
-   
+    Hull: ${ENEMY.hull} Firepower: ${ENEMY.firepower} Accuracy: ${ENEMY.accuracy}`);    
+}
+
+attack = () => {
     
 }
 
@@ -43,5 +50,10 @@ randomizeStat = (min, max) => {
     return Math.random() * (max - min) + min;
 }
 
+const NEW_GAME = true;
 const GAME_BTN = document.getElementById("game-btn");
 GAME_BTN.addEventListener("click", startGame);
+
+const ATTACK_BTN = document.createElement("button");
+ATTACK_BTN.innerHTML = "ATTACK!"
+ATTACK_BTN.addEventListener("click", attack);
